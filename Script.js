e58e663f1e2585aee4456330e1791ecc36f6c122
DanielSarf <72676273+DanielSarf@users.noreply.github.com>
2022-05-18 17:00:40 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,55 @@
+var audio = new Audio('sound/scribble.wav');
+
+nameAndScoreTemplate = document.getElementsByClassName("nameAndScore");
+
+namesAndScoresContainer = document.getElementById("namesAndScoresContainer");
+
+var cachedData = [];
+
+setInterval(fetchData, 1000);
+
+function fetchData()
+{
+    fetch("/leaderboard.json")
+        .then(response => response.json())
+        .then(data => {
+            if(!(JSON.stringify(data) === cachedData))
+            {
+                cachedData = JSON.stringify(data);
+                
+                sortByScore(data);
+                
+                audio.play();
+
+                refreshLeaderBoard(data);
+            }
+        });
+}
+
+function sortByScore(data)
+{
+    for (nameAndScore in data)
+    {
+        for (nameAndScore in data)
+        {
+            if(nameAndScore != 0 && data[nameAndScore].score > data[(nameAndScore - 1)].score)
+            {
+                [ data[nameAndScore], data[nameAndScore - 1] ] = [ data[nameAndScore - 1], data[nameAndScore] ];
+            }
+        }   
+    }
+}
+
+function refreshLeaderBoard(data)
+{
+    namesAndScoresContainer.innerHTML = "";
+
+    for (nameAndScore in data)
+    {
+        namesAndScoresContainer.appendChild(nameAndScoreTemplate[0].cloneNode(true));
+
+        namesAndScoresContainer.lastChild.getElementsByClassName("name")[0].getElementsByTagName("h3")[0].innerText = data[nameAndScore].name;
+
+        namesAndScoresContainer.lastChild.getElementsByClassName("score")[0].getElementsByTagName("h3")[0].innerText = data[nameAndScore].score;
+    }
+}
